184d4ba378844f3e77ae668a1d631319f97730f3
Vartika upadhyay <vartikau3@gmail.com>
2026-06-15 12:11:23 +0530

    Updated project

--- TCA.py
+++ TCA.py
@@ -26,14 +26,65 @@
  #   if box.startswith("a") or box.startswith("b"):
   #      print(box)
    
-str1= "listen"    
-str2= "silent"
+#str1 = "listen"
+#str2 = "silent"
 
-result = yes
+#if len(str1) != len(str2):
+#   print("No")
+#  else:
+#  for ch in str1:
+#      if ch in temp:
+#         temp.remove(ch)
+#       else:
+#          print("No")
+#         break
+#else:
+#   print("Yes")  
+     
+#str1 = "listen"
+#str2 = "silent"
+#str2_list = list(str2)
+#result = "yes"
 
-for i in range:
+#for i in range(len(str1)):
+ #   char=str1[i]
+  #  found_match=False
+   # for j in range(len(str2_list)):
+    # if char==str2[j]
+     #found_match=False
+    #else:
+     #   found not match
     
-            
+# random_string=""
+# count_variable=[]
+
+# for i in range(len(random_string)):
+#     count = 0
+#     for j in range(len(random_string)):
+#    if(random_string[i])!= count_variable[j]:
+
+        
+
+
+random_string = "My name is akshat"
+
+count_variable = []
+count_number = []
+
+for ch in random_string:
+    if ch not in count_variable:
+        count = 0
+
+        for x in random_string:
+            if ch == x:
+                count += 1
+
+        count_variable.append(ch)
+        count_number.append(count)
+
+print(count_variable)
+print(count_number)
+        
     
 
     
